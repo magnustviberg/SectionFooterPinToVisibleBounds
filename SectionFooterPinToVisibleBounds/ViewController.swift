c6a39cc1b8664e934f20c8c8bd54ecf7dd050bc1
Magnus Tviberg <magnus.tviberg@nrk.no>
2020-09-08 15:16:05 +0200

    added cells and footer

--- SectionFooterPinToVisibleBounds/ViewController.swift
+++ SectionFooterPinToVisibleBounds/ViewController.swift
@@ -10,11 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+
     }
 
 
 }
 
+// MARK: - Datasource
+extension ViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        5
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "test"
+        return cell
+    }
+
+}
+
+// MARK: - Delegate
+extension ViewController: UITableViewDelegate {
+
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        40
+    }
+
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 50))
+        footerView.backgroundColor = .red
+        let label = UILabel()
+        label.text = "footer"
+        label.sizeToFit()
+        footerView.addSubview(label)
+        label.center = CGPoint(x: footerView.frame.width / 2, y: footerView.frame.height / 2)
+        return footerView
+    }
+
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        50
+    }
+}
