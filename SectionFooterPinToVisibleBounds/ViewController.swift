b5511e7711dd992976b8150b0156edf66523cdf2
Magnus Tviberg <magnus.tviberg@nrk.no>
2020-09-08 14:45:20 +0200

    Initial Commit

---
+++ SectionFooterPinToVisibleBounds/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SectionFooterPinToVisibleBounds
+//
+//  Created by Magnus Tviberg on 08/09/2020.
+//  Copyright © 2020 Purpl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
